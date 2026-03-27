9fe28559d19b0cb9674bb89626276e7c784521c6
jucasoliveira <jucas.oliveira@gmail.com>
2026-03-27 23:09:22 +0000

    Implement player thrust mechanics and update project settings

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movements : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustForce = 20f;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        thrust.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            // add force of 10 upwards
+            rb.AddForce(Vector3.up * thrustForce * Time.fixedDeltaTime, ForceMode.Impulse);
+            Debug.Log("Thrusting");
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        thrust.Disable();
     }
 }
